d64bb05efe03a6ae64c9c1cf7fa0c64132dd192f
kuanysh-zhaksylyk <kuan.zhk@gmail.com>
2023-10-09 18:18:26 +0200

    feature(common): adding dag

--- dags/dag_with_mysql.py
+++ dags/dag_with_mysql.py
@@ -4,25 +4,17 @@ from airflow.providers.mysql.operators.mysql import MySqlOperator
 
 default_args = {
     "owner": "kuanysh",
-    "retries": 5, 
+    "retries": 5,
     "retry_delay": timedelta(minutes=2)
 }
 
-with DAG(
-    dag_id="mysql_etl_dag",
-    default_args=default_args,
-    start_date=datetime(2023, 10, 9),
-    schedule_interval='@daily'
-) as dag:
-    task1 = MySqlOperator(
-        task_id="mysql_operator",
-        mysql_conn_id="mysql_connection",
-        sql="""
-        CREATE TABLE Staff (
-        id INT,
-        name VARCHAR(255) NOT NULL,
-        position VARCHAR(30),
-        birthday Date);
-        """
-    )
-    task1
+dag = DAG('simple-mysql-dag',
+          default_args=default_arg,
+          schedule_interval='0 0 * * *')
+
+mysql_task = MySqlOperator(dag=dag,
+                           mysql_conn_id='mysql_default',
+                           task_id='mysql_task',
+                           sql='mysql/db_dump_version_2.sql')
+
+mysql_task
